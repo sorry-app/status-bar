0131baedfc7db605b5a669f1b6b0eee7d6464983
Robert Rawlins <robert@weboffins.com>
2013-11-06 15:11:47 +0000

    Changed the namespacing on the template, so it now doesn't copy that of Bootstrap, which will avoid collision of styles I hope.

--- src/javascripts/sorry-announcer.js
+++ src/javascripts/sorry-announcer.js
@@ -3,13 +3,13 @@
 $(document).ready(function() {
 	// Validate we have all the required elements for the plugin.
 	// We need a data attribute of the page ID before we can continue.
-	if (!$("body").attr('data-apology-announcements')) throw new Error('You must set a data attribute on the body tag for apology-announcements which contains the ID of your Sorry status page.')
+	if (!$("body").attr('data-sorry-announcement')) throw new Error('You must set a data attribute on the body tag for sorry-announcement which contains the ID of your Sorry status page.')
 
 	// Set the configurable variables.
 	// The page ID is used in the API calls that we make, and any chanels for PUSH subscription.
 	// We pull this from a data attirbute in the HTML, on the body tag.
 	// TODO: Should we support multiple pages here in future?
-	var page_id = $('body').data('apology-announcements');
+	var page_id = $('body').data('sorry-announcement');
 
 	// Reference the dismissed items, if none in local storage then assume new array.
 	// TODO: What is local storage is not available?
@@ -18,7 +18,7 @@ $(document).ready(function() {
 	// Set the HTML template for the notices we're going to add.
 	// Also include a link to the status page in here.
 	// This is based on a Bootstrap alert. http://getbootstrap.com/components/#alerts
-	var template = '<div class="alert alert-status alert-warning alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><span class="alert-text"></span> <a target="_blank" class="alert-link"></a></div>';
+	var template = '<div class="sorry-announcement"><button type="button" class="close" aria-hidden="true">&times;</button><span class="sorry-announcement-text"></span> <a target="_blank" class="sorry-announcement-link"></a></div>';
 
 	// Make a JSON request to acquire any apologies to display.
 	$.ajax({
@@ -38,11 +38,11 @@ $(document).ready(function() {
 					var $template = $(template)
 
 					// Assign an ID to the DOM element - we reference this later on to remember when dismissed.
-					$template.attr('id', 'apology-' + apology.id);
+					$template.attr('id', 'sorry-announcement-' + apology.id);
 					// Swap out the content in the template.
-					$template.find('.alert-text').text(apology.description);
+					$template.find('.sorry-announcement-text').text(apology.description);
 					// Update the link to the apology
-					$template.find('.alert-link').attr('href', apology.link).text(apology.link);
+					$template.find('.sorry-announcement-link').attr('href', apology.link).text(apology.link);
 
 					// Append the template to the DOM.
 					// We put this at the begining of the <body> tag so it's at the top of the DOM.
@@ -57,11 +57,11 @@ $(document).ready(function() {
 	});
 
 	// Bind the close event on any of the alerts which are added.
-	$('body').on('click', '.alert-status .close', function() {
+	$('body').on('click', '.sorry-announcement .close', function() {
 		// Target the parent element of the close button.
 		var target = $(this).parent();
 		// Get the native numeric ID from the element.
-		var id = target.attr('id').split('-')[1];
+		var id = target.attr('id').split('-')[2];
 		// Remember the ID which we are dismissing by putting it in the array
 		dismissed.push(id);
 		// Put that array in a serialized form in to local storage.
