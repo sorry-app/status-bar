a03bf6320a9b7f382cbfc79eded789dd248dbb0b
Robert Rawlins <robert@weboffins.com>
2013-11-22 13:35:15 +0000

    Added some basic tests for the underlying plugin setup, stolen from the Bootstrap plugins.

--- tests/unit/status-bar.js
+++ tests/unit/status-bar.js
@@ -1,4 +1,28 @@
-// Default hello world test.
-test( "hello test", function() {
-	ok( 1 == "1", "Passed!" );
+$(function () {
+
+	// Module for the status bar plugin.
+	module("status-bar");
+
+		test("should provide no conflict", function () {
+			// See if it response to noConflict call.
+			var status_bar = $.fn.statusBar.noConflict();
+
+			// Assert the result we wanted.
+			ok(!$.fn.statusBar, 'status bar was set back to undefined (org value)');
+
+			// Reset the value.
+			$.fn.statusBar = status_bar;
+		});
+
+		test("should be defined on jquery object", function () {
+			// Ensure that it's applied as a jQuery plugin.
+			ok($(document.body).statusBar, 'status-bar method is defined');
+		});
+
+		test("should return element", function () {
+			// Ensure that it binds to an element as expected.
+			// Pass in the constructor argument for the test.
+			ok($(document.body).statusBar('payme')[0] == document.body, 'document.body returned');
+		});
+
 });
