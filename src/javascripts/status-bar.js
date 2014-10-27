bf2abd2146d6bf29cb096ae702a1b660243eaee1
Robert Rawlins <robert@weboffins.com>
2014-10-27 16:57:26 +0000

    Namespaced the setup method.

--- src/javascripts/status-bar.js
+++ src/javascripts/status-bar.js
@@ -168,21 +168,6 @@
 
 	// jQuery Plugin Definition.
 
-	// Preload the DOM elements.
-	$.fn.setup = function() {
-		// Loop over all pages assigned on the including script tag.
-		// TODO: Can we shorthand this somehow?
-		// TODO: Can we abstract this out into a seperate metho?
-		$($('script[src$="status-bar.min.js"]')[0].getAttribute("data-for").split(",")).each(function() {
-			// Check to see if a status bar locator is present.
-			if($('[data-status-bar-for="' + this + '"]').length === 0)
-				// We don't have a container / locator for our status bar
-				// so we need to inject one into the DOM near the opening
-				// body tag.
-				$('body').prepend('<div class="sorry-status-bar" data-status-bar-for="' + this + '"></div>');
-		});
-	};
-
 	// Reference the noflict version.
 	var old = $.fn.statusBar;
 
@@ -207,12 +192,27 @@
 		return this;
 	};
 
+	// Preload the DOM elements.
+	$.fn.statusBar.setup = function() {
+		// Loop over all pages assigned on the including script tag.
+		// TODO: Can we shorthand this somehow?
+		// TODO: Can we abstract this out into a seperate metho?
+		$($('script[src$="status-bar.min.js"]')[0].getAttribute("data-for").split(",")).each(function() {
+			// Check to see if a status bar locator is present.
+			if($('[data-status-bar-for="' + this + '"]').length === 0)
+				// We don't have a container / locator for our status bar
+				// so we need to inject one into the DOM near the opening
+				// body tag.
+				$('body').prepend('<div class="sorry-status-bar" data-status-bar-for="' + this + '"></div>');
+		});
+	};
+
 	// Data-Api
 
 	// Instantiate the plugin on window load.
 	$(window).on('load', function () {
 		// Preload the statusbar DOM elements.
-		$.fn.setup();
+		$.fn.statusBar.setup();
 
 		// Attach the plugin to the body tag.
 		$('[data-status-bar-for]').each(function () {
