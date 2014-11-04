17a8c6a4f0a54b4a2ef81fc882df3332bfbba42c
Robert Rawlins <robert@weboffins.com>
2014-11-04 16:51:11 +0000

    Fixed a buf whereby an empty status bar container was being added to the DOM on plugin init even if data-for wasn't provided on the script tag.

--- src/javascripts/status-bar.js
+++ src/javascripts/status-bar.js
@@ -266,19 +266,21 @@
 	$.fn.statusBar.setup = function() {
 		// Determine any pages assigned to the script tag.
 		// Default to no pages if we don't find any.
-		var pages = $('script[src$="status-bar.min.js"]')[0].getAttribute("data-for") || '';
+		var pages = $('script[src$="status-bar.min.js"]')[0].getAttribute("data-for");
 
 		// Loop over all pages assigned on the including script tag.
 		// TODO: Can we shorthand this somehow?
 		// TODO: Can we abstract this out into a seperate metho?
-		$(pages.split(",")).each(function() {
-			// Check to see if a status bar locator is present.
-			if($('[data-status-bar-for="' + this + '"]').length === 0)
-				// We don't have a container / locator for our status bar
-				// so we need to inject one into the DOM near the opening
-				// body tag.
-				$('body').prepend('<div class="sorry-status-bar" data-status-bar-for="' + this + '"></div>');
-		});
+		if(pages) {
+			$(pages.split(",")).each(function() {
+				// Check to see if a status bar locator is present.
+				if($('[data-status-bar-for="' + this + '"]').length === 0)
+					// We don't have a container / locator for our status bar
+					// so we need to inject one into the DOM near the opening
+					// body tag.
+					$('body').prepend('<div class="sorry-status-bar" data-status-bar-for="' + this + '"></div>');
+			});
+		}
 	};
 
 	// Data-Api
