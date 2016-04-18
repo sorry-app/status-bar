9b31a7a1350e1cf55360ecabd3d0ad0bc30fc37f
Robert Rawlins <robrawlins@gmail.com>
2016-04-18 11:53:39 +0100

    Initial prototype of the subscriber being pass through to the API from a config/options object stored in the window.

--- src/javascripts/status-bar.js
+++ src/javascripts/status-bar.js
@@ -14,10 +14,28 @@
 
 	var SorryAPI = function(options) {
 		// Quick self refernce to the class.
-		var self = this;		
+		var self = this;
 
-		// Set a reference to the endpoint.
-		self.endpoint = 'https://ro-api.sorryapp.com/v1';
+		// Cache options into the class.
+		// Merge in options from the defaults, followed by
+		// the window object and the local hash if passed.
+		self.options = $.extend({}, SorryAPI.DEFAULTS, window.SorryAPIOptions, options);
+	};
+
+	SorryAPI.DEFAULTS = {
+		host: 'https://ro-api.sorryapp.com',
+		version: 1
+	};
+
+	// Provide an easy accessor for the endpoint URL.
+	SorryAPI.prototype.endpoint_url = function() {
+		// Reference self again.
+		var self = this;
+
+		// Compile the endpoint from the host and version
+		// number available in the options.
+		// TODO Cater for inproperly formatted hosts and version numbrs.
+		return self.options.host + '/v' + self.options.version;
 	};
 
 	// TODO: Add support for success/fail behaviour.
@@ -26,7 +44,7 @@
 		var self = this;
 
 		// Compile the target URL from the parameters.
-		var target_url = self.endpoint + '/pages/' + page_id;
+		var target_url = self.endpoint_url() + '/pages/' + page_id;
 
 		// Make a JSON request to acquire any notices to display.
 		return $.ajax({
@@ -35,7 +53,10 @@
 			dataType: "json",
 			url: target_url,
 			headers: { 'X-Plugin-Ping': 'status-bar' },
-			data: { include: 'brand,notices,notices.updates' },
+			data: { 
+				include: 'brand,notices,notices.updates', // Get brand and notices in a sigle package.
+				subscriber: self.options.subscriber // Pass optional subscriber configured in the client.
+			},
 			success: callback
 		});
 	};
