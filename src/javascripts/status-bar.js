99da70b4a477bb2e37118d20f8a116752665f6b8
Robert Rawlins <robrawlins@gmail.com>
2017-04-06 09:56:48 +0100

    Fixed a quirk which meant the 'more' link wasn't adopting the brand link-color on it's border, and only it's text.

--- src/javascripts/status-bar.js
+++ src/javascripts/status-bar.js
@@ -232,6 +232,7 @@
 				color: {{text_color}}; \
 			}\
 			.sorry-status-notice-link, .sorry-status-notice-link:hover { \
+				border-color: {{link_color}}; \
 				color: {{link_color}}; \
 			} \
 			.sorry-status-notice-close, .sorry-status-notice-close:hover { \
