3abc99100f553daa170a0bbdf6b2e6e79286fe5e
Robert Rawlins <robrawlins@gmail.com>
2017-05-04 14:07:23 +0100

    Changed the instantiation process on the plugin so that it copies accross data attributes to the div from the script tag.

--- dist/status-bar.js
+++ dist/status-bar.js
@@ -44141,23 +44141,33 @@ module.exports = {
 
 	// Preload the DOM elements.
 	$.fn.statusBar.setup = function() {
+		// Get a reference to the script tag including the plugin.
+		var script_tag = $($('script[src$="status-bar.min.js"]')[0]);
 		// Determine any pages assigned to the script tag.
 		// Default to no pages if we don't find any.
-		var pages = $('script[src$="status-bar.min.js"]')[0].getAttribute("data-for");
+		var pages = script_tag.data("for") || "";
 
 		// Loop over all pages assigned on the including script tag.
-		// TODO: Can we shorthand this somehow?
-		// TODO: Can we abstract this out into a seperate metho?
-		if(pages) {
-			$(pages.split(",")).each(function() {
-				// Check to see if a status bar locator is present.
-				if($('[data-status-bar-for="' + this + '"]').length === 0)
-					// We don't have a container / locator for our status bar
-					// so we need to inject one into the DOM near the opening
-					// body tag.
-					$('body').prepend('<div class="sorry-status-bar" data-status-bar-for="' + this + '"></div>');
-			});
-		}
+		// TODO: Can we abstract this out into a separate method?
+		$(pages.split(",")).each(function() {
+			// Check to see if a status bar locator is present.
+			if($('[data-status-bar-for="' + this + '"]').length === 0) {
+				// We don't have a container / locator for our status bar
+				// so we need to inject one into the DOM near the opening
+				// body tag.
+				var div_tag = $('<div />', {
+					// Set the class on the new tag.
+					'class': 'sorry-status-bar',
+					// Copy the reference to the status page.
+					'data-status-bar-for': this,
+					// Copy the other data attributes.
+					// TODO: Can we dynamically copy all of them?
+					'data-filter-type': script_tag.data("filter-type"),
+					'data-filter-components': script_tag.data("filter-components"),
+				// Attach it to the body.
+				}).prependTo('body');
+			}
+		});
 	};
 
 	// Data-Api
