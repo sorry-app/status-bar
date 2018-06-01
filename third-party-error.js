029e6e2cacb626d26a69b63c8861d1724f9a3dd0
Robert Rawlins <robrawlins@gmail.com>
2018-06-01 10:09:00 +0100

    Added some mock third-party errors to the dev example so we can check that console warnings, errors and promise rejections aren't caught by our version of Sentry and cause broken stack traces.

---
+++ third-party-error.js
@@ -0,0 +1,31 @@
+/*
+ * A mock third-party JavaScript lib to test that the website plugin
+ * isn't conflicting with other peoples code.
+ */
+
+(function(window, document, undefined) { "use strict";
+
+    /* Generic error message. */
+    var error_msg = 'Third-party library error, stack track should NOT show as from status-bar.js'
+
+    /*
+     * Raise a warning/error to ensure that the
+     * plugin still runs, and doesn't log the
+     * the exception.
+     */
+    // Console warning.
+    console.warn(error_msg);
+    
+    // Uncaught promise rejections.
+    Promise.reject(new Error(error_msg)).then(function(result) {
+        // Promise resolved.
+        console.log('Promise was resolved');
+    }, function(result) {
+        // Promise rejected.
+        console.log(result);
+    });
+    
+    // Proper exception.
+    throw new Error(error_msg);
+
+})(window, document);
